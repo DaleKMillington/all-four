39ee200fe7c8b0a56fa48dfad59a87a4c443add3
Dale Millington <dkmillington@tutanota.com>
2024-11-14 19:24:47 +0000

    Initial sass directory structure with resets and placeholder font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import './App.scss'
-
 export const App = () => {
   return (
     <p>Setup test</p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+// Third Party
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// Project
 import { App } from './App.tsx'
+import './scss/main.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
