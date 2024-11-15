62c5c6e5546066a30584ce038d2c80957c362927
Dale Millington <dkmillington@tutanota.com>
2024-11-15 17:58:45 +0000

    Refactored/removed unneeded components ready for MVP

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,6 @@
 // Local
 import './game.scss';
 import { Board } from '../components/board/Board';
-import { StatePanel } from '../components/statePanel/StatePanel';
 
 // Component
 export const Game = () => {
@@ -9,9 +8,6 @@ export const Game = () => {
         <div className='game'>
             <div className="game__column">
                 <Board />
-            </div>
-            <div className="game__column">
-                <StatePanel />
             </div>
         </div>
     );
