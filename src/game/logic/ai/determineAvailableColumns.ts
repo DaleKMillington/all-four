60773165761dc0ca5141829a70a49f6d9df20b69
Dale Millington <dkmillington@tutanota.com>
2024-11-16 21:00:51 +0000

    Added shuffle to the available columns to stop AI favouring the left of the board

--- src/game/logic/ai/determineAvailableColumns.ts
+++ src/game/logic/ai/determineAvailableColumns.ts
@@ -2,8 +2,17 @@
 import { cellColors, CellColorsType } from "../../../constants/cellColors.const";
 
 // Declarations
+const shuffleArray = (array: number[]): number[] => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
 export const determineAvailableColumns = (cells: CellColorsType[][]): number[] => {
-    return cells[0]
+    const availableColumns = cells[0]
         .map((cell, colIndex) => (cell === cellColors.clear ? colIndex : -1))
         .filter(colIndex => colIndex !== -1);
+    return shuffleArray(availableColumns);
 };
