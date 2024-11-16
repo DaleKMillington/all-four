8dc798bbe716c42bfad05a29730d6ad3bd23ac47
Dale Millington <dkmillington@tutanota.com>
2024-11-16 11:20:35 +0000

    Moved game provider up the app level

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // Project
 import { Menu } from "./components/menu/Menu";
 import { Game } from "./game/Game";
+import { GameProvider } from "./game/GameContext";
 
 // Component
 export const App = () => {
   return (
-    <>
+    <GameProvider>
         <Menu />
         <Game /> 
-    </>
+    </GameProvider>
   );
 };
--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,17 +1,14 @@
 // Local
 import './game.scss';
 import { Board } from '../components/board/Board';
-import { GameProvider } from './GameContext';
 
 // Component
 export const Game = () => {
     return (
-        <GameProvider>
-            <div className='game'>
-                <div className="game__column">
-                    <Board />
-                </div>
+        <div className='game'>
+            <div className="game__column">
+                <Board />
             </div>
-        </GameProvider>
+        </div>
     );
 };
