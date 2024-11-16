31488a6af71ab063ecd28afc86bf578550394307
Dale Millington <dkmillington@tutanota.com>
2024-11-16 15:46:31 +0000

    Have pencilled in my heuristics as comments. Will work on filling these in now.

--- src/game/logic/handleAIMove.ts
+++ src/game/logic/handleAIMove.ts
@@ -4,6 +4,24 @@ import { actions } from "../state/gameState/GameState";
 
 // Declarations
 export const handleAIMove = ({gameState, dispatch}: GameContextValueType) => {
+    // I am going to design this AI to follow a set of heuristics based on priority.
+    // Due to time constraints this is not going to be super intelligent.
+
+    // 1. If there is a winning move - take it! 
+
+    // 2. If there is a move that would block a winning move - take it!
+
+    // 3. If there is a move that gurantees 2 possible winning moves in the next turn - take it!
+
+    // 4. If there is a move that blocks the other player from a guranteed 2 possible winning moves next turn - take it!
+
+    // 5. If there is a move that gives 3 connected pieces - take it!
+
+    // 6. If there is a move that gives 2 connected pieces - take it!
+
+    // 7. If none of the above are satisfied - then favour as close to the center of the board.
+
+    // THIS IS JUST A PLACEHOLDER AND WILL BE REMOVED!
     // Test a random move just to set up the flow.
     const randomNumber = Math.floor(Math.random() * 7);
 
